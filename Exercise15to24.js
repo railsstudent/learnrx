5de9accdd2666893bd2653516a425678fc7beb63
Connie Leung <cleung334@gmail.com>
2017-05-30 20:11:54 +0800

    Complete exercise 24
    User array.zip static function to combine middle interesting moment and smallest boxart

--- Exercise15to24.js
+++ Exercise15to24.js
@@ -1,3 +1,20 @@
+Array.prototype.filter = function(predicateFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Apply the predicateFunction to each item in the array.
+		// If the result is truthy, add the item to the results array.
+		// Note: remember you can add items to the array using the array's
+		// push() method.
+		// ------------ INSERT CODE HERE! ----------------------------
+	  if (predicateFunction(itemInArray)) {
+      results.push(itemInArray);
+    }
+  });
+
+	return results;
+};
+
 Array.prototype.map = function(projectionFunction) {
 	var results = [];
 	this.forEach(function(itemInArray) {
@@ -377,3 +394,162 @@ Array.zip = function(left, right, combinerFunction) {
 
 console.log(JSON.stringify(Array.zip([1,2,3],[4,5,6], function(left, right) { return left + right })) === '[5,7,9]');
 		
+
+/*
+Exercise 23: Combine videos and bookmarks by index
+
+Let's repeat exercise 21, but this time lets use your new zip() function. For 
+each video and bookmark pair, create a {videoId, bookmarkId} pair.
+*/
+var ex23 = function() {
+	var videos = [
+			{
+				"id": 70111470,
+				"title": "Die Hard",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 4.0,
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 5.0,
+			},
+			{
+				"id": 65432445,
+				"title": "The Chamber",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 4.0,
+			},
+			{
+				"id": 675465,
+				"title": "Fracture",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": 5.0,
+			}
+		],
+		bookmarks = [
+			{id: 470, time: 23432},
+			{id: 453, time: 234324},
+			{id: 445, time: 987834}
+		];
+
+	return Array.
+		zip(videos, bookmarks, 
+			function(video, bookmark) { 
+				return { videoId: video.id, bookmarkId: bookmark.id }; 
+			}); 
+}
+console.log(ex23());
+
+/* 
+Exercise 24: Retrieve each video's id, title, middle interesting moment time, and smallest box art url.
+
+This is a variation of the problem we solved earlier. This time each video has 
+an interesting moments collection, each representing a time during which a 
+screenshot is interesting or representative of the title as a whole. Notice that 
+both the boxarts and interestingMoments arrays are located at the same depth in 
+the tree. Retrieve the time of the middle interesting moment and the smallest 
+box art url simultaneously with zip(). Return an {id, title, time, url} objec
+t for each video.
+*/
+var ex24 = function() {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxarts": [
+							{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"interestingMoments": [
+							{ type: "End", time:213432 },
+							{ type: "Start", time: 64534 },
+							{ type: "Middle", time: 323133}
+						]
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxarts": [
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+							{ width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"interestingMoments": [
+							{ type: "End", time:54654754 },
+							{ type: "Start", time: 43524243 },
+							{ type: "Middle", time: 6575665}
+						]
+					}
+				]
+			},
+			{
+				name: "Instant Queue",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxarts": [
+							{ width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"interestingMoments": [
+							{ type: "End", time:132423 },
+							{ type: "Start", time: 54637425 },
+							{ type: "Middle", time: 3452343}
+						]
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxarts": [
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+							{ width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+							{ width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"interestingMoments": [
+							{ type: "End", time:45632456 },
+							{ type: "Start", time: 234534 },
+							{ type: "Middle", time: 3453434}
+						]
+					}
+				]
+			}
+		];
+
+	//------------ COMPLETE THIS EXPRESSION --------------
+	return movieLists.
+		concatMap(function(movieList) {
+				return movieList.videos.concatMap(function(video) {
+							var smallestBoxart = video.boxarts
+								.reduce(function(acc, boxart) {
+									return (acc.width * acc.height) < (boxart.width * boxart.height) ? acc : boxart;
+								});
+								
+							var middleMoment = video.interestingMoments
+								.filter(function(m) {
+									return m.type === "Middle";
+								});
+							return Array.zip(middleMoment, smallestBoxart, function(l, r) {
+								return { id: video.id, title: video.title, time: l.time, url: r.url};
+							});	
+				});
+		});
+}
+console.log(ex24());
