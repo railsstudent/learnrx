b671f19ffd5621040ac0ba68e8256afdd84677a1
Connie Leung <cleung334@gmail.com>
2017-05-30 17:19:21 +0800

    Complete exercise 6 - 11
    Use map, filter and concat all to manipulate arrays and objects

--- Exercise6to11.js
+++ Exercise6to11.js
@@ -3,8 +3,305 @@
 Like projection, filtering an array is also a very common operation. 
 To filter an array we apply a test to each item in the array and collect the 
 items that pass into a new array.
+*/
+
+/*
 Exercise 6: Use forEach() to collect only those videos with a rating of 5.0
 
 Use forEach() to loop through the videos in the newReleases array and, if a 
 video has a rating of 5.0, add it to the videos array. 
 */
+function ex6() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	],
+	videos = [];
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Use forEach function to accumulate every video with a rating of 5.0
+	// ------------ INSERT CODE HERE! -----------------------------------
+  newReleases.forEach(function(n) {
+     if (n.rating === 5) {
+       videos.push(n);
+     }
+  });
+	return videos;
+}
+console.log(ex6())
+
+/* Exercise 7: Implement filter()
+
+To make filtering easier, let's add a filter() function to the Array type. 
+The filter() function accepts a predicate. A predicate is a function that accepts 
+an item in the array, and returns a boolean indicating whether the item should 
+be retained in the new array.
+*/
+Array.prototype.filter = function(predicateFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Apply the predicateFunction to each item in the array.
+		// If the result is truthy, add the item to the results array.
+		// Note: remember you can add items to the array using the array's
+		// push() method.
+		// ------------ INSERT CODE HERE! ----------------------------
+	  if (predicateFunction(itemInArray)) {
+      results.push(itemInArray);
+    }
+  });
+
+	return results;
+};
+
+// JSON.stringify([1,2,3].filter(function(x) { return x > 2})) === "[3]"
+console.log(JSON.stringify([1,2,3].filter(function(x) { return x > 2})) === "[3]");
+
+
+/* Query Data by Chaining Method Calls
+Exercise 8: Chain filter and map to collect the ids of videos that have a rating of 5.0
+*/
+var ex8 = function() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	];
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Chain the filter and map functions to select the id of all videos
+	// with a rating of 5.0.
+  return newReleases.filter(function(n) {
+    return n.rating === 5;
+  }).map(function(n) {
+    return n.id;
+  });
+	// ------------ INSERT CODE HERE! -----------------------------------
+}
+console.log(ex8());
+
+
+/* Querying Trees
+Sometimes, in addition to flat arrays, we need to query trees. Trees pose a challenge 
+because we need to flatten them into arrays in order to apply filter() and map() operations 
+on them. In this section we'll define a concatAll() function that we can combine 
+with map() and filter() to query trees.
+*/
+
+/*
+Exercise 9: Flatten the movieLists array into an array of video ids
+Let's start by using two nested forEach loops to collect the id of every video 
+in the two-dimensional movieLists array.
+*/
+var ex9 = function() {
+	var movieLists = [
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		},
+		{
+			name: "Dramas",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		}
+	],
+	allVideoIdsInMovieLists = [];
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+	// Use two nested forEach loops to flatten the movieLists into a list of
+	// video ids.
+	// ------------   INSERT CODE HERE!  -----------------------------------
+  movieLists.forEach(function(movie) {
+    movie.videos.forEach(function(video) {
+      allVideoIdsInMovieLists.push(video.id);
+    })
+  });
+	return allVideoIdsInMovieLists;
+
+}
+console.log(ex9());
+
+/*
+Exercise 10: Implement concatAll()
+
+Let's add a concatAll() function to the Array type. The concatAll() function 
+iterates over each sub-array in the array and collects the results in a new, 
+flat array. Notice that the concatAll() function expects each item in the array 
+to be another array
+*/
+Array.prototype.concatAll = function() {
+	var results = [];
+	this.forEach(function(subArray) {
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Add all the items in each subArray to the results array.
+		// ------------ INSERT CODE HERE! ----------------------------
+    if (Array.isArray(subArray)) {
+	    results = results.concat(subArray);
+    } else {
+      //throw "element is not an array";
+    }
+  });
+	return results;
+};
+
+// JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()) === "[1,2,3,4,5,6,7,8,9]"
+// [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
+console.log(JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()) === "[1,2,3,4,5,6,7,8,9]");
+try {
+  console.log([1,2,3].concatAll());
+} catch (ex) {
+  console.log(ex);
+}
+
+/*
+Exercise 11: Use map() and concatAll() to project and flatten the movieLists 
+into an array of video ids
+
+Hint: use two nested calls to map() and one call to concatAll().
+*/
+var ex11 = function() {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id: 432534, time: 65876586 }]
+					}
+				]
+			},
+			{
+				name: "Dramas",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id: 432534, time: 65876586 }]
+					}
+				]
+			}
+		];
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+	// Use map and concatAll to flatten the movieLists in a list of video ids.
+	// ------------   INSERT CODE HERE!  -----------------------------------
+  return movieLists.map(function(movie) {
+          return movie.videos.map(function(video) { return video.id; })
+        }).concatAll();
+}
+console.log(ex11());
